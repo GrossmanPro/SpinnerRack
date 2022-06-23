6117044ef200e482c3bc0d76ae9bfb38647c7ffa
GrossmanPro <GrossmanProject@outlook.com>
2022-06-22 22:31:33 -0400

    Fixed namespace bug when catching Exception & PDOException objects

--- src/DbConnection.php
+++ src/DbConnection.php
@@ -13,22 +13,35 @@ class DbConnection {
     // https://phpdelusions.net/pdo/common_mistakes
     
     public $connection;
-
+    
+    /**
+     * function __construct
+     * Parameters match the arguments needed to create a PDO object.
+     * @param string $dsn
+     * @param string $username
+     * @param string $password
+     */
     public function __construct(string $dsn, string $username, string $password) {
         $this->dsn = $dsn;
         $this->username = $username;
         $this->password = $password;
     }
 
+    /**
+     * function connect
+     * The Exception and PDO Exception must be caught with a backslash
+     * because they're outside the src namespace defined in this project.
+     * @return boolean
+     */
     public function connect() {
         try {
             $this->connection = new PDO($this->dsn, $this->username, $this->password);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             print $e->getMessage();
             error_log($e->getMessage());
             error_log($e->getTraceAsString());
             return false;
-        } catch (PDOException $p) {
+        } catch (\PDOException $p) {
             print $p->getMessage();
             error_log($p->getMessage());
             error_log($p->getTraceAsString());
