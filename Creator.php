2d0c86cdd3dfd3d473fcd97cc8b3708ef17bdf79
GrossmanPro <GrossmanProject@outlook.com>
2022-06-22 20:13:15 -0400

    Creator class

---
+++ Creator.php
@@ -0,0 +1,69 @@
+<?php
+class Creator
+{
+    private $id;
+    private $firstName;
+    private $lastName;
+    protected $connection;
+    
+    public function __construct(object $db, int $id = 0) {        
+        $this->connection = $db;
+        $this->id = $id;           
+        if ($this->id) {
+            $this->loadCreator();
+        }
+    }
+    
+    public function getFirstName(): string 
+    {
+        return $this->firstName;
+    }
+    
+    public function setFirstName(string $firstName) {
+        $this->firstName = filter_var($firstName, FILTER_SANITIZE_STRING);
+    }
+    
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+    
+    public function setLastName(string $lastName) {
+        $this->lastName = filter_var($lastName, FILTER_SANITIZE_STRING);
+    }
+    
+    public function getFullName(bool $lastNameFirst = false): string
+    {
+        if ($lastNameFirst) {
+            return $this->lastName . ', ' . $this->firstName;
+        } else {
+            return $this->firstName . ' ' . $this->lastName;
+        }
+    }
+    
+    public function loadCreator() {
+        if ($this->id) {
+            $sql = 'SELECT * FROM Creators WHERE Id = :id';
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute(array($id));
+            $creator = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+            $this->firstName = $creator['FirstName'];
+            $this->lastName = $creator['LastName'];      
+        } else {
+            return false;
+        }
+    }      
+  
+    public function saveCreator() {
+        if ($this->id) {
+            $sql = 'UPDATE Creators SET FirstName = :FirstName, LastName = :LastName WHERE Id = :ID';
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute(array($this->firstName, $this->lastName, $this->id));
+        } else {
+            $sql = 'INSERT INTO Creators (FirstName, LastName) VALUES (:FirstName, :LastName)';
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute(array($this->firstName, $this->lastName));
+        }
+    }    
+}
+
