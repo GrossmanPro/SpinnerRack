a15fb41798d7c2df368be25b53fb999380ca6833
GrossmanPro <GrossmanProject@outlook.com>
2022-06-25 10:29:35 -0400

    New unit tests for Creator class.

--- src/tests/CreatorTest.php
+++ src/tests/CreatorTest.php
@@ -57,19 +57,19 @@ class CreatorTest extends TestCase {
         $pdo = null;
     }
     
-    public function testLoadCreatorSuccess() {
+    public function testLoadCreatorByConstructorSuccess() {
         $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
         $username = "SpinnerRackUser";
         $password = "password";
         $pdo = new PDO($dsn, $username, $password);
         $creator = new Creator($pdo, 1);
-        // John Byrne is Creator 1
+        // with id, John Byrne is Creator 1
         $this->assertEquals("Byrne", $creator->getLastName());
         $this->assertEquals("John", $creator->getFirstName());
         $pdo = null;
     }
     
-    public function testLoadCreatorFail() {
+    public function testLoadCreatorByConstructorFail() {
         $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
         $username = "SpinnerRackUser";
         $password = "password";
@@ -81,5 +81,30 @@ class CreatorTest extends TestCase {
         $pdo = null;
     }
 
-
+    public function testLoadCreatorNoId() {
+        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
+        $username = "SpinnerRackUser";
+        $password = "password";
+        $pdo = new PDO($dsn, $username, $password);
+        $creator = new Creator($pdo);
+        $this->assertEquals("", $creator->getLastName());
+        $this->assertEquals("", $creator->getFirstName());
+    }
+    
+    public function testLoadCreatorByIdSuccess() {
+        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
+        $username = "SpinnerRackUser";
+        $password = "password";
+        $pdo = new PDO($dsn, $username, $password);
+        $creator = new Creator($pdo);
+        // John Byrne is Creator 1
+        $creator->loadCreatorById(1);
+        $this->assertEquals("Byrne", $creator->getLastName());
+        $this->assertEquals("John", $creator->getFirstName());
+        // Jim Starlin is Creator 5
+        $creator->loadCreatorById(5);
+        $this->assertEquals("Starlin", $creator->getLastName());
+        $this->assertEquals("Jim", $creator->getFirstName());        
+    }
+    
 }
