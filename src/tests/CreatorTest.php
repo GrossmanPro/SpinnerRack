fea3edd6e9d8c0023f7e7f4ee28d1f38d69d3045
GrossmanPro <GrossmanProject@outlook.com>
2022-06-25 22:09:15 -0400

    Switched to a global $pdo object in test class...Not ideal, but closer.

--- src/tests/CreatorTest.php
+++ src/tests/CreatorTest.php
@@ -1,49 +1,43 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-require '../Creator.php';
+require_once '../DbConfig.php';
+require_once '../DbConn.php';
+require_once '../Creator.php';
 
 class CreatorTest extends TestCase {
     
-    // remember to use test prefix, e.g. testSetFirstNameSuccess
+    // remember to use test prefix, e.g. testSetFirstNameSuccess   
+    
     /**
      * @covers ::setFirstName
      */
     public function testSetFirstNameSuccess() {
         // TODO
         // How do I pass a db connection to this class?
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator($pdo);
         $creator->setFirstName("Eric");
         $this->assertEquals("Eric", $creator->getFirstName());
-        $pdo = null;
+        //$pdo = null;
     }
 
     /**
      * @covers ::setFirstName
      */
     public function testSetFirstNameFail() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator($pdo);
         $creator->setFirstName("Eric");
         $this->assertFalse("Elvis" === $creator->getFirstName());
-        $pdo = null;
+        //$pdo = null;
     }
     
     /**
      * @covers ::getFullName
      */
     public function testGetFullNameSuccess() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         // Jim Starlin is Creator 5
         $creator = new Creator();
         $creator->loadCreatorById($pdo, 5);
@@ -55,40 +49,27 @@ class CreatorTest extends TestCase {
      * @covers ::setLastName
      */
     public function testSetLastNameSuccess() {
-        // TODO
-        // How do I pass a db connection to this class?
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator($pdo);
         $creator->setLastName("Grossman");
         $this->assertEquals("Grossman", $creator->getLastName());
-        $pdo = null;
     }
 
     /**
      * @covers ::setLastName
      */
     public function testSetLastNameFail() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator($pdo);
         $creator->setLastName("Grossman");
         $this->assertFalse("Presley" === $creator->getLastName());
-        $pdo = null;
     }
 
     /**
      * @covers ::__construct
      */
     public function testLoadCreatorNoId() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator($pdo);
         $this->assertEquals("", $creator->getLastName());
         $this->assertEquals("", $creator->getFirstName());
@@ -98,10 +79,7 @@ class CreatorTest extends TestCase {
      * @covers ::loadCreatorById
      */
     public function testLoadCreatorByIdSuccess() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator();
         // John Byrne is Creator 1
         $creator->loadCreatorById($pdo, 1);
@@ -117,33 +95,25 @@ class CreatorTest extends TestCase {
      * @covers ::loadCreatorById
      */
     public function testLoadCreatorByIdFail() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator();
         try {
             $creator->loadCreatorById($pdo, 0);
         } catch (Exception $e) {
             $this->assertEquals("This creator does not exist", $e->getMessage());
         }
-        $pdo = null;
     }
     
     /**
      * @covers ::saveCreator
      */
     public function testSaveCreatorNewSuccess() {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator();
         $creator->setFirstName("Delete");
-        $creator->setLastName("Me6");
+        $creator->setLastName("Me11");
         $id = $creator->saveCreator($pdo);
         $this->assertTrue(ctype_digit($id));
-        $pdo = null;
         return $id;
     }
     
@@ -152,17 +122,13 @@ class CreatorTest extends TestCase {
      * @covers ::saveCreator
      */
     public function testSaveCreatorUpdateSuccess(string $id) {
-        $dsn = "sqlsrv:Server=DESKTOP-6PN824R\SQLEXPRESS;Database=SpinnerRack;";
-        $username = "SpinnerRackUser";
-        $password = "password";
-        $pdo = new PDO($dsn, $username, $password);
+        global $pdo;
         $creator = new Creator();
         $creator->loadCreatorById($pdo, $id);
         $creator->setFirstName("Joe");
         $id = $creator->saveCreator($pdo);
         $this->assertTrue(ctype_digit($id));
         $this->assertEquals("Joe", $creator->getFirstName());
-        $pdo = null;
     }
     
 }
