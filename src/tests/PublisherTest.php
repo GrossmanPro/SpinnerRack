7154ef8a6d43fc902839b003a83a90c6f5dba26f
GrossmanPro <GrossmanProject@outlook.com>
2022-06-26 12:39:17 -0400

    PHPUnit test class for Publisher

---
+++ src/tests/PublisherTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once '../DbConfig.php';
+require_once '../DbConn.php';
+require_once '../Publisher.php';
+
+class PublisherTest extends TestCase {
+    
+    // remember to use test prefix, e.g. testSetFirstNameSuccess 
+    
+    /**
+     * @covers ::savePublisher
+     * @covers ::setPublisherName
+     * @covers ::getPublisherName
+     */
+    public function testSavePublisherNewSuccess() {
+        global $pdo;
+        $publisher = new Publisher();
+        $publisher->setPublisherName("Delete Pub");
+        $id = $publisher->savePublisher($pdo);
+        $this->assertTrue(ctype_digit($id));
+        $this->assertEquals("Delete Pub", $publisher->getPublisherName());
+        return $id;
+    }
+    /**
+     * @depends testSavePublisherNewSuccess
+     * @covers ::loadPublisherById
+     * @covers ::savePublisher
+     * @covers ::getPublisherName
+     */
+    public function testSavePublisherUpdateSuccess(string $id) { 
+        global $pdo;
+        $publisher = new Publisher();
+        $publisher->loadPublisherById($pdo, (int)$id);
+        $publisher->setPublisherName("Delete This Publisher");
+        $idPostSave = $publisher->savePublisher($pdo);  
+        $this->assertTrue(is_int($idPostSave));    
+        $this->assertEquals("Delete This Publisher", $publisher->getPublisherName());
+    }
+    
+}
