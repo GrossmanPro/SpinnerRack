452725e9c9c53ea2e8809ee703b31d542114d080
GrossmanPro <GrossmanProject@outlook.com>
2022-07-03 22:42:32 -0400

    Code to dynamically add/remove multiple artists or writers from the comic form

---
+++ js/ComicFormView.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#submitComic").click(function() {
+        $("#comicInput").submit();
+    });    
+    
+    var artistCount = 0;
+    $("#artists").change(function(){
+        artistCount++;
+        var artistName = $("#artists :selected").text();
+        var artistId = $("#artists :selected").val();
+        
+        var html = '<div class="input-group" id="artistDiv_' + artistCount + '">';
+        html += '<input type="text" class="form-control-plaintext" value="' + artistName + '" readonly>';
+        html += '<input type="button" class="btn btn-danger removeCreator" value="Delete">';
+        html += '<input type="hidden" id="artist_' + artistCount + '" name="artist_' + artistCount + '" value="' + artistId + '">';
+        html += '<div>';
+        $("#artistList").append(html);
+    });
+    
+    var scripterCount = 0;
+    $("#scripters").change(function(){
+        scripterCount++;
+        var scripterName = $("#scripters :selected").text();
+        var scripterId = $("#scripters :selected").val();
+        
+        var html = '<div class="input-group" id="scripterDiv_' + scripterCount + '">';
+        html += '<input type="text" class="form-control-plaintext" value="' + scripterName + '" readonly>';
+        html += '<input type="button" class="btn btn-danger removeCreator" value="Delete">';
+        html += '<input type="hidden" id="scripter_' + scripterCount + '" name="scripter_' + scripterCount + '" value="' + scripterId + '">';
+        html += '<div>';
+        $("#scripterList").append(html);
+    });  
+    
+    $("body").on("click", ".removeCreator", function(){
+        $(this).closest("div").remove();
+    });
+});
