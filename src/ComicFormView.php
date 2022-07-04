1b7145005dea1f952199394b53f4119ed03dfc84
GrossmanPro <GrossmanProject@outlook.com>
2022-07-04 12:45:34 -0400

    Created functions to draw standard page headers and footers

--- src/ComicFormView.php
+++ src/ComicFormView.php
@@ -12,94 +12,85 @@ require_once 'FormHelpers.php';
 $titleOptions = getSelectListOptions($pdo, 'Titles', 'OptionText');
 $creatorOptions = getSelectListOptions($pdo, 'Creators', 'OptionText');
 
+print drawHeader('Save Comic');
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Save Comic</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  </head>
-  <body>
-      <div class="container">
-          <form id="comicInput" method="post" action="ComicFormController.php">
-              <div class="row mb-3">
-                  <div class="form-floating col-md-6">
-                      <select class="form-select" id="title" name="title" title="Comic title" autofocus>
-                          <?php print $titleOptions; ?>
-                      </select>
-                      <label for="title">Title</label>
-                  </div>
-                  <div class="form-floating col-md-2">
-                      <input type="text" class="form-control" id="issue" name="issue" title="Issue number" required>
-                      <label for="issue">Issue No</label>
-                  </div>  
-                  <div class="form-floating col-md-2">
-                      <select class="form-select" id="month" name="month" title="Month of publication">
-                          <option value="1">JAN</option>
-                          <option value="2">FEB</option>
-                          <option value="3">MAR</option>
-                          <option value="4">APR</option>
-                          <option value="5">MAY</option>
-                          <option value="6">JUN</option>
-                          <option value="7">JUL</option>
-                          <option value="8">AUG</option>
-                          <option value="9">SEP</option>
-                          <option value="10">OCT</option>
-                          <option value="11">NOV</option>
-                          <option value="12">DEC</option>
-                      </select>
-                      <label for="month">Cover month</label>
-                  </div>
-                  <div class="form-floating col-md-2">
-                      <input type="text" class="form-control" id="year" name="year" maxlength="4" min="1930" max="2100" title="Year of publication" required>
-                      <label for="year">Year</label>
-                  </div>
-              </div>
-              <div class="row mb-3">
-                  <div class="form-floating col-md-6">
-                    <input type="text" class="form-control" id="story" name="story" title="Story" required>
-                    <label for="story">Story</label>
-                  </div>              
-                  <div class="form-floating col-md-2">
-                      <select class="form-select" id="stars" name="stars" title="Comic rating">
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                      </select>
-                      <label for="stars">Rating</label>
-                  </div>
-              </div>
-              <div class="row mb-3">
-                  <div class="form-floating col-md-6">
-                      <textarea class="form-control" rows="4" id="notes" name="notes" title="Story notes"></textarea>
-                      <label for="notes">Notes</label>
-                  </div>                      
-              </div>    
-              <div class="row mb-3">
-                  <div class="form-floating col-md-4">
-                      <select class="form-select" id="scripters" name="scripters" title="Comic scripter or scripters">
-                          <?php print $creatorOptions; ?>
-                      </select>
-                      <label for="title">Writer(s)</label>
-                  </div>
-                  <div class="form-floating col-md-4">
-                      <select class="form-select" id="artists" name="artists" title="Comic artist or artists">
-                          <?php print $creatorOptions; ?>
-                      </select>
-                      <label for="title">Artist(s)</label>
-                  </div>
-              </div>
-                <div class="row">
-                  <input type="button" class="btn btn-primary" id="submitComic" value="Save Comic">
-                </div>
-          </form>
-      </div>
-    <script src="/js/jquery.js"></script>
-    <script src="/js/ComicFormView.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  </body>
-</html>
+<div class="container">
+    <form id="comicInput" method="post" action="ComicFormController.php">
+        <div class="row mb-3">
+            <div class="form-floating col-md-6">
+                <select class="form-select" id="title" name="title" title="Comic title" autofocus>
+                    <?php print $titleOptions; ?>
+                </select>
+                <label for="title">Title</label>
+            </div>
+            <div class="form-floating col-md-2">
+                <input type="text" class="form-control" id="issue" name="issue" title="Issue number" required>
+                <label for="issue">Issue No</label>
+            </div>  
+            <div class="form-floating col-md-2">
+                <select class="form-select" id="month" name="month" title="Month of publication">
+                    <option value="1">JAN</option>
+                    <option value="2">FEB</option>
+                    <option value="3">MAR</option>
+                    <option value="4">APR</option>
+                    <option value="5">MAY</option>
+                    <option value="6">JUN</option>
+                    <option value="7">JUL</option>
+                    <option value="8">AUG</option>
+                    <option value="9">SEP</option>
+                    <option value="10">OCT</option>
+                    <option value="11">NOV</option>
+                    <option value="12">DEC</option>
+                </select>
+                <label for="month">Cover month</label>
+            </div>
+            <div class="form-floating col-md-2">
+                <input type="text" class="form-control" id="year" name="year" maxlength="4" min="1930" max="2100" title="Year of publication" required>
+                <label for="year">Year</label>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="form-floating col-md-6">
+              <input type="text" class="form-control" id="story" name="story" title="Story" required>
+              <label for="story">Story</label>
+            </div>              
+            <div class="form-floating col-md-2">
+                <select class="form-select" id="stars" name="stars" title="Comic rating">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <label for="stars">Rating</label>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="form-floating col-md-6">
+                <textarea class="form-control" rows="4" id="notes" name="notes" title="Story notes"></textarea>
+                <label for="notes">Notes</label>
+            </div>                      
+        </div>    
+        <div class="row mb-3">
+            <div class="form-floating col-md-4">
+                <select class="form-select" id="scripters" name="scripters" title="Comic scripter or scripters">
+                    <?php print $creatorOptions; ?>
+                </select>
+                <label for="title">Writer(s)</label>
+                <div id="scripterList"></div>
+            </div>
+            <div class="form-floating col-md-4">
+                <select class="form-select" id="artists" name="artists" title="Comic artist or artists">
+                    <?php print $creatorOptions; ?>
+                </select>
+                <label for="title">Artist(s)</label>
+                <div id="artistList"></div>
+            </div>
+        </div>
+          <div class="row">
+            <input type="button" class="btn btn-primary" id="submitComic" value="Save Comic">
+          </div>
+    </form>
+</div>
+<?php
+print drawFooter(array('ComicFormView.js'));
--- src/FormHelpers.php
+++ src/FormHelpers.php
@@ -16,3 +16,48 @@ function getSelectListOptions(object $pdo, string $table, string $orderBy): stri
     return $options;
 }
 
+/**
+ * Function drawHeader
+ * Returns markup for standard page header.
+ * Optionally adds custom CSS files.
+ * @param string $title
+ * @param array $cssFiles
+ * @return string
+ */
+function drawHeader(string $title, array $cssFiles = null): string {
+    $header = '<!doctype html>';
+    $header .= '<html lang="en">';
+    $header .= '<head>';
+    $header .= '<meta charset="utf-8">';
+    $header .= '<meta name="viewport" content="width=device-width, initial-scale=1">';
+    $header .= '<title>' . $title . '</title>';
+    $header .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">';
+    if (!is_null($cssFiles)) {
+        foreach ($cssFiles as $file) {
+            $header .= '<link src="" rel="stylesheet">';
+        }
+    }
+    $header .= '</head>';
+    $header .= '<body>';
+    return $header;
+}
+
+/**
+ * Function drawFooter
+ * Returns markup for standard page footer.
+ * Optionally adds custom JavaScript files.
+ * @param array $jsFiles
+ * @return string
+ */
+function drawFooter(array $jsFiles = null): string {
+    $footer = '<script src="/js/jquery.js"></script>';
+    if (!is_null($jsFiles)) {
+        foreach ($jsFiles as $file) {
+            $footer .= '<script src="/js/' . $file . '"></script>';
+        }
+    }
+    $footer .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>';
+    $footer .= '</body>';
+    $footer .= '</html>';
+    return $footer;
+}
