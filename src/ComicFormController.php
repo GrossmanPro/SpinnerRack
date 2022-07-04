ed83968e2a7bf26cc6f89219934159920fdb97e8
GrossmanPro <GrossmanProject@outlook.com>
2022-07-04 12:07:12 -0400

    Server-side validation preventing the same Creator from being saved as writer or artist more than once on the same book

--- src/ComicFormController.php
+++ src/ComicFormController.php
@@ -7,37 +7,43 @@ require_once 'Title.php';
 require_once 'Creator.php';
 require_once 'Comic.php';
 
+
 extract($_POST); // filtered by Comic object methods
 
-$comic = new Comic();
-$comic->setTitleId($title);
-$comic->setIssue($issue);
-$comic->setMonth($month);
-$comic->setStars($stars);
-$comic->setStory($story);
-$comic->setNotes($notes);
-$comic->saveComic($pdo);
+try {
+    $comic = new Comic();
+    $comic->setTitleId($title);
+    $comic->setIssue($issue);
+    $comic->setMonth($month);
+    $comic->setStars($stars);
+    $comic->setStory($story);
+    $comic->setNotes($notes);
+    $comic->saveComic($pdo);
 
-$scripters = array_filter($_POST, function($key) {
-    return stripos($key, 'scripter_')!== false;
-}, ARRAY_FILTER_USE_KEY);
+    $scripters = array_unique(array_filter($_POST, function($key) {
+        return stripos($key, 'scripter_')!== false;
+    }, ARRAY_FILTER_USE_KEY));                           
 
-if (count($scripters)) {
-    foreach ($scripters as $key => $val) {
-        $comic->setScripter($pdo, $val);
+    if (count($scripters)) {
+        foreach ($scripters as $key => $val) {
+            $comic->setScripter($pdo, $val);
+        }
     }
-}
 
-$artists = array_filter($_POST, function($key) {
-    return stripos($key, 'artist_') !== false;
-}, ARRAY_FILTER_USE_KEY);
+    $artists = array_unique(array_filter($_POST, function($key) {
+        return stripos($key, 'artist_') !== false;
+    }, ARRAY_FILTER_USE_KEY));
 
-if (count($artists)) {
-    foreach ($artists as $key => $val) {
-        $comic->setArtist($pdo, $val);
+    if (count($artists)) {
+        foreach ($artists as $key => $val) {
+            $comic->setArtist($pdo, $val);
+        }
     }
+} catch (Exception $ex) {
+    error_log($ex->getMessage());
+    error_log($ex->getTraceAsString());
+    header('Location: GeneralError.php');
 }
-
 print 'SAVED!';
 
 
