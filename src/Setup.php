40058f64f4b97ac187d54a55720807772280a70d
GrossmanPro <GrossmanProject@outlook.com>
2022-07-08 19:02:24 -0400

    Combining basic files into single include file

---
+++ src/Setup.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'DbConfig.php';
+require_once 'DbConn.php';
+
+require_once 'Publisher.php';
+require_once 'Title.php';
+require_once 'Creator.php';
+require_once 'Comic.php';
+require_once 'FormHelpers.php';
+require_once 'AntiCsrf.php';
