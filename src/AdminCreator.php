eb895e7f7625ae6f51f600b834b8bd7f91810b3f
GrossmanPro <GrossmanProject@outlook.com>
2022-07-11 16:57:32 -0400

    New save creator functionality

--- src/AdminCreator.php
+++ src/AdminCreator.php
@@ -1,8 +1,33 @@
 <?php
 require_once 'Setup.php';
-print drawHeader('Admin: Creators');
-print '<div class="container">';
-print '<p class="h4">Admin: Creators</p>';
+print drawHeader('Admin: Creators');  
+
+if (array_key_exists('status', $_GET)) {
+    // show save ok message
+}
+
+$addForm = <<<EOD
+<div class="container pt-4">
+<p class="h4">Admin: Creators</p>
+<form id="comicInput" method="post" action="/admin/save">
+$tokenTag
+<div class="row mb-3 align-items-center">
+<div class="form-floating col-md-4">
+    <input type="text" class="form-control" id="firstName" name="firstName" title="Creator first name" required>
+    <label for="firstName">First Name</label>
+</div>  
+<div class="form-floating col-md-4">
+    <input type="text" class="form-control" id="lastName" name="lastName" title="Creator last name" required>
+    <label for="lasttName">Last Name</label>
+</div>  
+<div class="col-md-1">
+    <input type="submit" class="btn btn-primary" value="Add" title="Add creator">
+</div>
+</div>
+</form>
+EOD;
+
+print $addForm;
 print Creator::getCreatorTable($pdo);
 print '</div>';
 print drawFooter(array('AdminCreator.js'));
