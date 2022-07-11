406e0517e63afe28d88460ef6b47ed8ab59d9f57
GrossmanPro <GrossmanProject@outlook.com>
2022-07-10 20:36:03 -0400

    Page title update

--- src/AdminCreator.php
+++ src/AdminCreator.php
@@ -2,6 +2,7 @@
 require_once 'Setup.php';
 print drawHeader('Admin: Creators');
 print '<div class="container">';
+print '<p class="h4">Admin: Creators</p>';
 print Creator::getCreatorTable($pdo);
 print '</div>';
 print drawFooter(array('AdminCreator.js'));
