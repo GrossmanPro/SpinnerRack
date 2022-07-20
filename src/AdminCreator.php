48396d231a02e14eb57bb8bea80c96b130568865
GrossmanPro <GrossmanProject@outlook.com>
2022-07-20 17:22:38 -0400

    Added spacing to floating labels for appearance

--- src/AdminCreator.php
+++ src/AdminCreator.php
@@ -10,11 +10,11 @@ $tokenTag
 <div class="row mb-3 align-items-center">
 <div class="form-floating col-md-4">
     <input type="text" class="form-control" id="firstName" name="firstName" title="Creator first name" required>
-    <label for="firstName">First Name</label>
+    <label for="firstName">&nbsp;&nbsp;First Name</label>
 </div>  
 <div class="form-floating col-md-4">
     <input type="text" class="form-control" id="lastName" name="lastName" title="Creator last name" required>
-    <label for="lasttName">Last Name</label>
+    <label for="lasttName">&nbsp;&nbsp;Last Name</label>
 </div>  
 <div class="col-md-1">
     <input type="submit" class="btn btn-primary" value="Save" title="Save creator">
