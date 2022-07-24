d8326660a5c611112f8b0dfcfe07c8fc85f505aa
GrossmanPro <GrossmanProject@outlook.com>
2022-07-23 23:27:12 -0400

    Renamed js include for file revving/cache busting

--- src/FormHelpers.php
+++ src/FormHelpers.php
@@ -85,7 +85,7 @@ function drawHeader(string $title, array $cssFiles = null): string {
  * @return string
  */
 function drawFooter(array $jsFiles = null): string {
-    $footer = '<script src="/js/jquery.js"></script>';
+    $footer = '<script src="/js/jquery-01.js"></script>';
     $footer .= '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>';
     $footer .= '<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>';
     $footer .= '<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>';
