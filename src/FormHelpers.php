b2128c9c9bd5c8c3cd165402b94ec2f89afdc26c
GrossmanPro <GrossmanProject@outlook.com>
2022-07-25 18:54:06 -0400

    Added Comics link to Admin menu

--- src/FormHelpers.php
+++ src/FormHelpers.php
@@ -66,6 +66,7 @@ function drawHeader(string $title, array $cssFiles = null): string {
     $header .= '<li class="nav-item dropdown">';
     $header .= '<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Admin</a>';
     $header .= '<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">';
+    $header .= '<li><a class="dropdown-item" href="/admin/comics">Comics</a></li>';
     $header .= '<li><a class="dropdown-item" href="/admin/creators">Creators</a></li>';
     $header .= '<li><a class="dropdown-item" href="/admin/publishers">Publishers</a></li>';
     $header .= '<li><a class="dropdown-item" href="/admin/titles">Titles</a></li>';
@@ -76,8 +77,8 @@ function drawHeader(string $title, array $cssFiles = null): string {
     $header .= '<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Comics</a>';
     $header .= '<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">';
     $header .= '<li><a class="dropdown-item" href="/comics/add">Add</a></li>';
-    $header .= '<li><a class="dropdown-item" href="/admin/publishers">Search</a></li>';
-    $header .= '<li><a class="dropdown-item" href="/admin/titles">View</a></li>';
+    $header .= '<li><a class="dropdown-item" href="/comics/search">Search</a></li>';
+    $header .= '<li><a class="dropdown-item" href="/comics/view">View</a></li>';
     $header .= '</ul>';
     $header .= '</ul>';
     $header .= '</li>';
