623e01b088c752f1df75cc4390089b73e1faa3da
GrossmanPro <GrossmanProject@outlook.com>
2022-07-26 20:22:54 -0400

    Fixed accessibility issue (repeated id attribute)

--- src/FormHelpers.php
+++ src/FormHelpers.php
@@ -74,8 +74,8 @@ function drawHeader(string $title, array $cssFiles = null): string {
     $header .= '</li>';  
     
     $header .= '<li class="nav-item dropdown">';
-    $header .= '<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Comics</a>';
-    $header .= '<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">';
+    $header .= '<a class="nav-link dropdown-toggle" href="#" id="navbarComics" role="button" data-bs-toggle="dropdown" aria-expanded="false">Comics</a>';
+    $header .= '<ul class="dropdown-menu" aria-labelledby="navbarComics">';
     $header .= '<li><a class="dropdown-item" href="/comics/add">Add</a></li>';
     $header .= '<li><a class="dropdown-item" href="/comics/search">Search</a></li>';
     $header .= '<li><a class="dropdown-item" href="/comics/view">View</a></li>';
