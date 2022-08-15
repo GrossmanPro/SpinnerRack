db89d96b3824b65f05ffa076ead71e3f9a07624f
GrossmanPro <GrossmanProject@outlook.com>
2022-08-15 18:45:11 -0400

    LIKE clauses for comic search

--- src/ComicSearchResults.php
+++ src/ComicSearchResults.php
@@ -2,7 +2,21 @@
 
 require_once 'Setup.php';
 
+$keyword = filter_input(INPUT_POST, 'keyword', FILTER_SANITIZE_SPECIAL_CHARS);
+
+$sql = "SELECT * FROM ComicSearchView "
+        . "WHERE Story LIKE ? "
+        . "OR Notes LIKE ? "
+        . "OR Name LIKE ? "
+        . "OR Publisher LIKE ?";
+$params = array_fill(0, 4, '%' . $keyword . '%');
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 print drawHeader('Results');
-print_r($_POST);
+
+print_r($results);
+
 
 print drawFooter();
